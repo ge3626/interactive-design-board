5ea422c808c3978758707ccb7215c6359fe773c7
Sarah Yoo(유가은) <gaeunyoo3626@gmail.com>
2024-03-14 11:03:33 -0400

    live cursor chat works

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,14 @@
 import { useMyPresence, useOthers } from "@/liveblocks.config"
 import LiveCursor from "./cursor/LiveCursor"
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
+import CursorChat from "./cursor/CursorChat";
+import { CursorMode } from "@/types/type";
 
 const Live = () => {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+    const [cursorState, setCursorState] = useState({mode: CursorMode.Hidden});
 
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
         event.preventDefault();
@@ -16,7 +20,7 @@ const Live = () => {
     }, []);
 
     const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-        event.preventDefault();
+        setCursorState({mode: CursorMode.Hidden});
         updateMyPresence({ cursor: null, message: null });
     }, []);
 
@@ -27,6 +31,35 @@ const Live = () => {
         updateMyPresence({ cursor: {x, y} });
     }, []);
 
+    useEffect(() => {
+        const onKeyUp = (e: KeyboardEvent) => {
+            if(e.key === '/') {
+                setCursorState({
+                    mode: CursorMode.Chat,
+                    previousMessage: null,
+                    message: '',
+                });
+            } else if (e.key === 'Escape') {
+                updateMyPresence({message: ''});
+                setCursorState({mode: CursorMode.Hidden});
+            }
+        }
+
+        const onKeyDown = (e: KeyboardEvent) => {
+            if(e.key === '/') {
+                e.preventDefault();
+            }
+        }
+
+        window.addEventListener('keyup', onKeyUp);
+        window.addEventListener('keydown', onKeyDown);
+
+        return () => {
+            window.removeEventListener('keyup', onKeyUp);
+            window.removeEventListener('keydown', onKeyDown);
+        }
+    }, [updateMyPresence]);
+
   return (
     <div 
         onPointerMove={handlePointerMove} 
@@ -34,6 +67,14 @@ const Live = () => {
         onPointerDown={handlePointerDown}
         className="h-[100vh] w-full flex justify-center items-center text-center"
     >      
+        {cursor && (
+            <CursorChat 
+                cursor={cursor}
+                cursorState={cursorState}
+                setCursorState={setCursorState}
+                updateMyPresence={updateMyPresence}
+            />
+        )}
         <h1  className="text-2xl text-white">Test Works</h1>
         <LiveCursor others={others}/>
     </div>
