61d70f28208da6ff190a252e0f88b1183e89208c
Sarah Yoo(유가은) <gaeunyoo3626@gmail.com>
2024-03-13 22:36:43 -0400

    live cursor works

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import { Room } from "./Room";
+"use client";
+
+import Live from "@/components/Live";
+
 
 
 export default function Page() {
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center text-center">
-    <Room>
-      <h1  className="text-2xl text-white">Test Works</h1>
-    </Room>
+    <div>
+      <Live />
     </div>
   );
 }
