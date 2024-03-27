21fdf42595778cf9d2db0990ea7ae9a5a9226e03
Sarah Yoo(유가은) <gaeunyoo3626@gmail.com>
2024-03-27 15:27:53 -0400

    real-time canvas works

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { defaultNavElement } from "@/constants";
+import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvaseMouseMove, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
+import { handleDelete } from "@/lib/key-events";
+import { useMutation, useStorage } from "@/liveblocks.config";
 import { ActiveElement } from "@/types/type";
+import { root } from "postcss";
 import { useState, useEffect, useRef } from "react";
 
 export default function Page() {
@@ -14,11 +18,54 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>('rectangle');
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const canvasObjects = useStorage((root) => (root.canvasObjects));
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if(!object) return;
+
+    const { objectId } = object;
+
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get('canvasObjects');
+    canvasObjects.set(objectId, shapeData);
+  }, []);
 
   const [activeElement, setActiveElement] = useState<ActiveElement>({name: '', value: '', icon: ''});
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get('canvasObjects');
+    if(!canvasObjects || canvasObjects.size === 0) return true;
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+    return canvasObjects.size === 0;
+  }, []);
+
+  const deleteShapeFromStorage = useMutation(({ storage }, objectId) => {
+    const canvasObjects = storage.get('canvasObjects');
+    canvasObjects.delete(objectId);
+  }, []);
+
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
+
+    switch(elem?.value) {
+      case 'reset':
+        deleteAllShapes();
+        fabricRef.current?.clear();
+        setActiveElement(defaultNavElement);
+        break;
+      case 'delete':
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        setActiveElement(defaultNavElement);
+      default:
+        break;
+    }
+
     selectedShapeRef.current = elem?.value as string;
   }
 
@@ -35,10 +82,52 @@ export default function Page() {
       });
     });
 
+    canvas.on("mouse:move", (options) => {
+      handleCanvaseMouseMove({
+        options, 
+        canvas, 
+        selectedShapeRef, 
+        isDrawing, 
+        shapeRef,
+        syncShapeInStorage
+      });
+    });
+
+    canvas.on("mouse:up", (options) => {
+      handleCanvasMouseUp({
+        canvas, 
+        selectedShapeRef, 
+        isDrawing, 
+        shapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef,
+      });
+    });
+
+    canvas.on("object:modified", (options) => {
+      handleCanvasObjectModified({
+        options,
+        syncShapeInStorage
+      });
+    });
+
     window.addEventListener("resize", () => {
       handleResize({ fabricRef });
     });
+
+    return () => {
+      canvas.dispose();
+    }
   }, []);
+
+  useEffect(() => {
+    renderCanvas({
+      fabricRef,
+      canvasObjects,
+      activeObjectRef
+    });
+  }, [canvasObjects]);
 
   return (
     <main className="h-screen overflow-hidden">
