931b87acfd02c6976365116f885fba2e06e22608
Sarah Yoo(유가은) <gaeunyoo3626@gmail.com>
2024-03-29 20:55:45 -0400

    image upload works & right sidebar added

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,9 +1,21 @@
+
 import React from 'react'
+import Dimensions from './settings/Dimensions'
+import Export from './settings/Export'
+import Color from './settings/Color'
+import Text from './settings/Text'
 
 const RightSidebar = () => {
   return (
-    <section className='flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky left-0 overflow-y-auto pb-20'>
+    <section className='flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-none'>
     <h3 className='px-5 t-4 text-xs uppercase'>Design</h3>
+    <span className='text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4'>Make changes to canvas as you like.</span>
+
+    <Dimensions />
+    <Text />
+    <Color />
+    <Color />
+    <Export />
     </section>
   )
 }
